0164a7940a1b04c60a56e7a7c16e5d2e8f712b5d
Bryan King <brynkng@gmail.com>
2013-07-01 23:57:58 -0400

    finished data gather script. will collect all bus info every 5 minutes

--- data-gatherer.rb
+++ data-gatherer.rb
@@ -6,10 +6,43 @@ require 'json'
 
 conn = PG.connect( dbname: 'septa_bus_alert' ) 
 
-uri = URI.parse("http://www3.septa.org/hackathon/TransitView/17")
+while true
+	busNumbers = (1..206).to_a + ('G'..'R').to_a
 
-response = Net::HTTP.get_response(uri)
-puts response.body	
+	busNumbers.each do |busNum|
+		puts busNum
+		uri = URI.parse("http://www3.septa.org/hackathon/TransitView/" + busNum.to_s)
+
+		response = Net::HTTP.get_response(uri)
+
+
+		if response.body != 'Invalid Route'
+			response = JSON.parse response.body
+			
+			#response
+			# ['bus'][array of buses][lat | lng | VehicleId | BlockId | Direction | destination | Offset]
+			response['bus'].each do |bus|
+				bus['route'] = busNum.to_s
+				if bus['destination'] != ""
+					conn.exec("INSERT INTO bus_history (route, latitute, longitude, vehicle_id, block_id, direction, destination, off_by) VALUES ('" \
+						+ bus['route'] + "','" \
+						+ bus['lat'] + "','" \
+						+ bus['lng'] + "','" \
+						+ bus['VehicleID'] + "','" \
+						+ bus['BlockID'] + "','" \
+						+ bus['Direction'] + "','" \
+						+ bus['destination'] + "','" \
+						+ bus['Offset'] + "')"
+					)
+				end
+			end		
+		end
+	end
+
+	sleep 5 * 60
+end
+
+
 
 #find out general bus times and only grab data during those times so as to not waste space
 
@@ -17,7 +50,14 @@ puts response.body
 # each lookup will be a combination of current time and location + the average history of previous amount of time to take to get to destination. 
 # each lookup should take detours into account
 
+#use color scheme 
+
 #probably need to use google maps api to get current walking time between two points.
+
+
+
+
+
 
 # conn.exec( "SELECT * FROM pg_stat_activity" ) do |result| 
 # 	puts " PID | User | Query" 
@@ -27,13 +67,14 @@ puts response.body
 # end
 
 # CREATE TABLE bus_history (
-#	id integer auto_increment,
-# 	time timestamp,
+# 	id serial,
+# 	time timestamp default NOW(),
+# 	route text,
 # 	latitute float,
 #   longitude float,
 #   vehicle_id integer,
 #   block_id integer,
-#   direction string,
-#   destination string,
-#   offset integer
-# )
+#   direction text,
+#   destination text,
+#   off_by integer
+# );
